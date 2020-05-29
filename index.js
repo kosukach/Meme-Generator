5702dc80dae159ed665dab846b6ef5384032c86e
Konstantin <kosukach@yahoo.com>
2020-05-29 18:19:59 +0300

    ready for deployment

--- index.js
+++ index.js
@@ -15,10 +15,14 @@ app.use(express.static("./public"));
 app.use(express.json());
 app.use("/memes", memes);
 
-mongoose.connect(config.get("db"), { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI || config.get("db"), { useNewUrlParser: true })
   .then(() => console.log(`Connected to ${config.get("db")}...`))
   .catch((ex)=> console.log(ex));
 
 
 let  port = process.env.PORT || 5000;
 app.listen(port, ()=> {console.log(`listening on port ${port}`);});
+
+if(process.env.NODE_ENV == "production"){
+  
+}
